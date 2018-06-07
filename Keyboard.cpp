c9b5fe9155d7bc40c839e5965140be9c53c56978
Vineeth Yeevani <vyeevani@berkeley.edu>
2018-06-07 19:15:00 +0000

    Automated build

--- Keyboard.cpp
+++ Keyboard.cpp
@@ -1,4 +1,5 @@
 
+
 #include "Keyboard.h"
 
 KeyboardDriver::KeyboardDriver(InterruptManager* interruptManager) : InterruptHandler(0x21, interruptManager), dataPort(0x60), commandPort(0x64) {
@@ -22,10 +23,18 @@ void printf(char* str);
 
 uint32_t KeyboardDriver::HandleInterrupt(uint32_t esp) {
   uint8_t key = dataPort.Read();
-  char* foo = "KeyBoard 0x00 ";
-  char* hex = "0123456789ABCDEF";
-  foo[11] = hex[(key >> 4) & 0x0F];
-  foo[12] = hex[(key & 0x0F)];
-  printf(foo);
+  if (key < 0x80) {
+    switch(key) {    
+      case 0xFA: break;
+      case 0x45: case 0xC5: break;
+      default:
+        char* foo = "KeyBoard 0x00 ";
+        char* hex = "0123456789ABCDEF";
+        foo[11] = hex[(key >> 4) & 0x0F];
+        foo[12] = hex[(key & 0x0F)];
+        printf(foo);
+    }
+  }
+  
   return esp;
 }
